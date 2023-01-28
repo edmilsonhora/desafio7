fb09412108515d015ebed76f5dc113eafe7da8ca
Edmilson Hora <edmilson_hora@yahoo.com.br>
2023-01-28 20:09:49 -0300

    Inicio

---
+++ src/ESH-Deasfio7.ApplicationService/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESH_Deasfio7.ApplicationService
+{
+    public class Class1
+    {
+    }
+}
